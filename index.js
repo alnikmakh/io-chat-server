a8aeb537c84770534a0b4e8ba40dd1c3d8c967f3
alnikmakh <alnikmakh@gmail.com>
2019-11-24 22:50:56 +0300

    Add handle chatrooms & add/remove online/disconnect users & update online users on clients

--- index.js
+++ index.js
@@ -7,14 +7,49 @@ const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
 
+let chatrooms = new Map();
+const addUserToChatroom = (idChatRoom, user) => {
+  let roomUsers = chatrooms.get(idChatRoom);
+  roomUsers.push(user);
+  chatrooms.set(idChatRoom, roomUsers);
+};
+
+const removeUserOfChatRoom = (idChatRoom, user) => {
+  let roomUsers = chatrooms.get(idChatRoom);
+  roomUsers.forEach((el, i) => {
+    if (el === user) {
+      roomUsers.splice(i, 1);
+    }
+  });
+  chatrooms.set(idChatRoom, roomUsers);
+};
+
 io.on("connection", socket => {
+  let nick = "";
+  let chatroom = "";
   const id = socket.client.id;
   
-    console.log(`User connected: ${id}`);
+  const updateOnlineUsers = (chatRoomId, usersOnline) => {
+    io.to(chatRoomId).emit("update", usersOnline);
+  };
+  
+  console.log(`User connected: ${id}`);
   
   socket.on("login", message => {
     console.log(message);
-    socket.join(message);
+    
+    nick = message.nick;
+    chatroom = message.id;
+    
+    socket.join(message.id);
+    
+    if (chatrooms.has(message.id)) {
+      addUserToChatroom(message.id, message.nick);
+      updateOnlineUsers(message.id, chatrooms.get(message.id));
+    } else {
+      chatrooms.set(message.id, [message.nick]);
+      updateOnlineUsers(message.id, chatrooms.get(message.id));
+    }
   });
 
   socket.on("chat message", msg => {
@@ -23,5 +58,9 @@ io.on("connection", socket => {
 
   socket.on("disconnect", (reason) => {
     console.log(`User disconnected: ${id}, reason: ${reason}`);
+    if (chatrooms.has(chatroom)) {
+      removeUserOfChatRoom(chatroom, nick);
+      updateOnlineUsers(chatroom, chatrooms.get(chatroom));
+    }
   })
 });
